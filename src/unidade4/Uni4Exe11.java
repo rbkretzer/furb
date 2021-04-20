ca2ccfcc3364cb48341ae88796aab05d7eaceda5
rbkretzer <74603751+rbkretzer@users.noreply.github.com>
2021-04-20 14:40:29 -0300

    Update Uni4Exe11.java

--- src/unidade4/Uni4Exe11.java
+++ src/unidade4/Uni4Exe11.java
@@ -7,21 +7,21 @@ public class Uni4Exe11 {
 	public static void main(String[] args) {
 
 		Scanner ler = new Scanner(System.in);
-		System.out.println("--------Digite os anos de nascimento dos irm„os--------");
-		System.out.print("Primeiro irm„o: ");
+		System.out.println("--------Digite os anos de nascimento dos irm√£os--------");
+		System.out.print("Primeiro irm√£o: ");
 		int ano1 = ler.nextInt();
-		System.out.print("Segundo irm„o: ");
+		System.out.print("Segundo irm√£o: ");
 		int ano2 = ler.nextInt();
-		System.out.print("Terceiro irm„o: ");
+		System.out.print("Terceiro irm√£o: ");
 		int ano3 = ler.nextInt();
 
 		if (ano1 == ano2 && ano1 == ano3) {
-			System.out.println("TRIG MIOS");
+			System.out.println("TRIG√äMIOS");
 		} else {
 			if (ano1 != ano2 && ano1 != ano3 && ano2 != ano3) {
-				System.out.println("APENAS IRM√OS");
+				System.out.println("APENAS IRM√ÉOS");
 			} else {
-				System.out.println("G MIOS");
+				System.out.println("G√äMEOS");
 			}
 		}
 
