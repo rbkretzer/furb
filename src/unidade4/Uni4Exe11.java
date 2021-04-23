f427ad1c5a6725bbb8a7c296f882144c61766dcf
rbkretzer <rafael.bkretzer@gmail.com>
2021-04-22 21:07:44 -0300

    Correção conflito

--- src/unidade4/Uni4Exe11.java
+++ src/unidade4/Uni4Exe11.java
@@ -7,30 +7,20 @@ public class Uni4Exe11 {
 	public static void main(String[] args) {
 
 		Scanner ler = new Scanner(System.in);
-		System.out.println("--------Digite os anos de nascimento dos irm√£os--------");
-		System.out.print("Primeiro irm√£o: ");
+		System.out.println("--------Digite os anos de nascimento dos irm„os--------");
+		System.out.print("Primeiro irm„o: ");
 		int ano1 = ler.nextInt();
-		System.out.print("Segundo irm√£o: ");
+		System.out.print("Segundo irm„o: ");
 		int ano2 = ler.nextInt();
-		System.out.print("Terceiro irm√£o: ");
+		System.out.print("Terceiro irm„o: ");
 		int ano3 = ler.nextInt();
 
 		if (ano1 == ano2 && ano1 == ano3) {
-<<<<<<< HEAD
 			System.out.println("TRIG MIOS");
 		} else if (ano1 != ano2 && ano1 != ano3 && ano2 != ano3) {
 			System.out.println("APENAS IRM√OS");
 		} else {
 			System.out.println("G MIOS");
-=======
-			System.out.println("TRIG√äMIOS");
-		} else {
-			if (ano1 != ano2 && ano1 != ano3 && ano2 != ano3) {
-				System.out.println("APENAS IRM√ÉOS");
-			} else {
-				System.out.println("G√äMEOS");
-			}
->>>>>>> 3d28beb93c5fb2fd72029ec4b8ba4684ea26ce8a
 		}
 
 		ler.close();
